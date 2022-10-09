769eb759bf7295f18c1150090c782ff67a2aa8c3
006400650062006\roger <rogermarti.campusnet@gmail.com>
2022-10-09 02:10:48 +0200

    Teleporter and Player Movement

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public float limiteX = 10;
+    public float limiteY = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > limiteY)
+        {
+            transform.position = new Vector3(transform.position.x, -limiteY);
+        }
+        if(transform.position.y < -limiteY)
+        {
+            transform.position = new Vector3(transform.position.x, limiteY);
+        }
+
+
+        if (transform.position.x > limiteX)
+        {
+            transform.position = new Vector3(-limiteX, transform.position.y);
+        }
+        if (transform.position.x < -limiteX)
+        {
+            transform.position = new Vector3(limiteX, transform.position.y);
+        }
+    }
+}
